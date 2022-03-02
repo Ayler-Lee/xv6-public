b95af41427c82564488efa1ad30a36bc21917644
Ayler <liyale@bixin.cn>
2022-03-02 12:31:03 +0800

    update test

--- lab2.c
+++ lab2.c
@@ -3,21 +3,26 @@
 #include "user.h"
 
 int main(int argc, char *argv[]) {
-    int pid = fork();
+    int pid1 = fork();
 
-    if (pid > 0) {
+    if (pid1 > 0) {
         int status;
         //  int cpid = wait(&status);
-        int wpid = waitpid(pid, &status, 0);
-        if (wpid == pid) {
-            printf(1, "child status: %d, pid: %d, cpid: %d\n", status, pid, wpid);
+        int pid2 = fork();
+        if (pid2 > 0) {
+            int wpid = waitpid(pid1, &status, 0);
+            if (wpid == pid1) {
+                printf(1, "child status: %d, pid: %d\n", status, wpid);
+            } else {
+                int status = 234;
+                printf(1, "child - %d exit: %d", pid2, status);
+                exitStatus(status);
+            }
         }
-         exit();
-    } else if (pid == 0) {
-        printf(1, "child sleeping");
-        sleep(5);
+        exit();
+    } else if (pid1 == 0) {
         int status = 123;
-        printf(1, "child exit: %d", status);
+        printf(1, "child - %d exit: %d", pid1, status);
         exitStatus(status);
     }
     exit();
