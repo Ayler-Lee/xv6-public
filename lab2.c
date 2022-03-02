d44c659f5018243feb6beb56c3448a5e50c3f158
Ayler <liyale@bixin.cn>
2022-03-02 12:32:17 +0800

    update test

--- lab2.c
+++ lab2.c
@@ -13,17 +13,17 @@ int main(int argc, char *argv[]) {
             int wpid = waitpid(pid1, &status, 0);
             if (wpid == pid1) {
                 printf(1, "child status: %d, pid: %d\n", status, wpid);
+                exit();
             } else {
                 int status = 234;
                 printf(1, "child - %d exit: %d", pid2, status);
                 exitStatus(status);
             }
         }
-        exit();
     } else if (pid1 == 0) {
         int status = 123;
         printf(1, "child - %d exit: %d", pid1, status);
         exitStatus(status);
     }
-    exit();
+    // exit();
 }
