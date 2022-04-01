392936fdb71ac89baa63a86add9f693e5062d6f1
Ayler <liyale@bixin.cn>
2022-04-01 20:07:39 +0800

    Step 4: aging of priority

--- proc.c
+++ proc.c
@@ -331,27 +331,36 @@ scheduler(void)
   for(;;){
     // Enable interrupts on this processor.
     sti();
-
+    struct proc *nextp;
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
+      if(!nextp) nextp = p;
+      if(p->state == RUNNABLE && nextp->priority > p->priority) {
+        if (nextp->priority > 0) 
+          nextp->priority -= 1;
+        nextp = p;
+      } else {
+        if(p->priority > 0)
+          p->priority -= 1;
+      }
     }
+    if (nextp->priority < 31) {
+      nextp->priority += 1;
+    }
+    // Switch to chosen process.  It is the process's job
+    // to release ptable.lock and then reacquire it
+    // before jumping back to us.
+    c->proc = p;
+    switchuvm(p);
+    p->state = RUNNING;
+
+    swtch(&(c->scheduler), p->context);
+    switchkvm();
+
+    // Process is done running for now.
+    // It should have changed its p->state before coming back.
+    c->proc = 0;
     release(&ptable.lock);
 
   }
