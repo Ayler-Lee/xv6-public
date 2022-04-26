7704df59b47045cfdfb11f563fad577438726ddb
Ayler <liyale@bixin.cn>
2022-04-26 21:02:28 +0800

    lab4

--- exec.c
+++ exec.c
@@ -63,10 +63,13 @@ exec(char *path, char **argv)
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
   sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+  // if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+  //   goto bad;
+  // clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
+  // sp = sz;
+  if((sp = allocuvm(pgdir, KERNBASE - 2*PGSIZE, KERNBASE-4)) == 0)
     goto bad;
-  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  sp = sz;
+  // clearpteu(pgdir, (char*)(KERNBASE - 2*PGSIZE));
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
@@ -97,6 +100,7 @@ exec(char *path, char **argv)
   oldpgdir = curproc->pgdir;
   curproc->pgdir = pgdir;
   curproc->sz = sz;
+  curproc->stacksz = KERNBASE - sp - 4;
   curproc->tf->eip = elf.entry;  // main
   curproc->tf->esp = sp;
   switchuvm(curproc);
