4660536ce1ec5c48a70f311856ea5754e3b1e37e
Ayler <liyale@bixin.cn>
2022-04-26 21:53:29 +0800

    copy stack

--- vm.c
+++ vm.c
@@ -339,21 +339,21 @@ copyuvm(pde_t *pgdir, uint sz)
   }
   cprintf("copy stack kernbase = %d down to %d", KERNBASE - 4, KERNBASE - 2*PGSIZE);
 
-  // for(i = KERNBASE - 4; i > KERNBASE - 2*PGSIZE; i -= PGSIZE){
-  //   if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
-  //     panic("copyuvm: pte should exist");
-  //   if(!(*pte & PTE_P))
-  //     panic("copyuvm: page not present");
-  //   pa = PTE_ADDR(*pte);
-  //   flags = PTE_FLAGS(*pte);
-  //   if((mem = kalloc()) == 0)
-  //     goto bad;
-  //   memmove(mem, (char*)P2V(pa), PGSIZE);
-  //   if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
-  //     kfree(mem);
-  //     goto bad;
-  //   }
-  // }
+  for(i = KERNBASE - 4; i > KERNBASE - 2*PGSIZE; i -= PGSIZE){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+      kfree(mem);
+      goto bad;
+    }
+  }
   return d;
 
 bad:
